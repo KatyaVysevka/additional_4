6a2d298141e47d14f1201e850fffe210632d0092
KatyaVysevka <katrina_v89@mail.ru>
2017-09-26 11:25:18 +0300

    new solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 module.exports = function multiply(first, second) {
   // your solution
   //
-  var multiply = Function ("x", "y", "return x * y");
 
- 
+  var z = (first * second);
+  var b = '';
+  for (; z >= 1;) {
+    var q = z % 10;
+    z = Math.trunc(z / 10);
+    b = String(q) + b;
+  }
+  return b;
+
 }
